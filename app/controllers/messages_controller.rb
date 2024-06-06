b0b4affa79c1a981c58ec8fab7b1627236141d7a
unknown <matumasie@gmail.com>
2024-06-06 21:01:17 +0000

    Add messages_controller.rb

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,22 @@
+class MessagesController < ApplicationController
+  before_action :set_chat_room
+
+  def create
+    if @chat_room.users.include?(current_user)
+      @message = @chat_room.messages.create!(message_params.merge(user: current_user))
+      redirect_to chat_path(@chat_room)
+    else
+      redirect_to root_path, alert: "You are not authorized to post messages in this chat room."
+    end
+  end
+
+  private
+
+  def set_chat_room
+    @chat_room = ChatRoom.find(params[:chat_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
