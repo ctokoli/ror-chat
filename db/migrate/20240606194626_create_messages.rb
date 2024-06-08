56215aabf66f76461807090e430fda8842c52e4d
unknown <matumasie@gmail.com>
2024-06-08 23:44:01 +0000

    Update 20240606194626_create_messages.rb

--- db/migrate/20240606194626_create_messages.rb
+++ db/migrate/20240606194626_create_messages.rb
@@ -1,9 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[7.1]
   def change
     create_table :messages do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :chat_room, null: false, foreign_key: {to_table: :users}
       t.text :content
-
+      
       t.timestamps
     end
   end
